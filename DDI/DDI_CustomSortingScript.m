bf92a398ffd5418a6357a88fb514bf5cad495443
Beatriz Padrela <beatrizpadrela@hotmail.com>
2021-04-26 12:33:05 +0200

    #4 DDI sorting script - solving merging issue

--- DDI/DDI_CustomSortingScript.m
+++ DDI/DDI_CustomSortingScript.m
@@ -2,8 +2,9 @@
 
 ExploreASL_Master('',0);
 
-Odir = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/DDI/ASL_dicoms';
-Ddir = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/DDI/sourcedata';
+BasePath = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/DDI';
+Odir = fullfile(BasePath,'ASL_dicoms');
+Ddir = fullfile(BasePath,'sourcedata');
 
 xASL_adm_CreateDir(Ddir);
 Slist1 = xASL_adm_GetFileList(Odir, '^D1\d{4}', 'FPList',[0 Inf], true);
@@ -73,10 +74,10 @@ for iT=1:length(Slist1) % There are more T1 files than ASL files, so Slist1 give
     txtdir = T1txtList{iT};
     readText=readTextFile(txtdir);
     txt=convert2subjectValuePairs(readText);
- 
+
     scannerlist{iT,1} = SubjNameT;
     scannerlist{iT,2} = SessionNameT;
-    scannerlist{iT,3} = [txt.Manufacturer '_' txt.ModelName '_' strrep(txt.SoftwareVersion,'/','\')];
+    scannerlist{iT,3} = [txt.Manufacturer '_' txt.ModelName '_' strrep(txt.SoftwareVersion,'\','_')];
    
     if ~isempty(strcmp(SessionNameT,Sessions))
         indT1 = find(strcmp(SessionNameT,Sessions));
@@ -84,7 +85,7 @@ for iT=1:length(Slist1) % There are more T1 files than ASL files, so Slist1 give
         T1DestDir = fullfile(Ddir,SubjNameT,SesT1,'T1');
         xASL_Copy(T1List{iT},T1DestDir,true); % Copies T1.nii file
         T1txtDestDir = fullfile(Ddir,SubjNameT,SesT1, 'T1.txt');
-        xASL_Copy(T1txtList{iT},T1DestDir,true); % Copies T1.txt also
+        xASL_Copy(T1txtList{iT},T1txtDestDir,true); % Copies T1.txt also
     end
 end
 
@@ -107,7 +108,7 @@ for iF=1:length(Slist1)
         FLAIRDestDir = fullfile(Ddir,SubjNameF,SesF, 'FLAIR');
         xASL_Copy(FLAIRList{iF},FLAIRDestDir,true); % Copies T1.nii file
         FLAIRtxtDestDir = fullfile(Ddir,SubjNameF,SesF, 'FLAIR.txt');
-        xASL_Copy(FLAIRtxtList{iF},FLAIRtxtDestDir,true); % Copies T1.txt also
+        xASL_Copy(FLAIRtxtList{iF},FLAIRtxtDestDir,true); % Copies T1.txt also 
     end
 end
 
@@ -115,7 +116,7 @@ end
 
 %xASL_io_DcmtkRead
 scanners = unique(scannerlist(:,3));
-root = '/home/bestevespadrela/lood_storage/divi/Projects/ExploreASL/DDI';
+root = BasePath;
 
 for iScanner = 1:length(scanners)
     
@@ -125,10 +126,10 @@ for iScanner = 1:length(scanners)
     xASL_adm_CreateDir(ScannerDest);
 
     % Copy all subjects that have this type to the directory
-    for iElement = 1:size(scannerList,1)
+    for iElement = 1:size(scannerlist,1)
         if strcmp(scannerlist{iElement,3},ScannerName)
             source = fullfile(Ddir,scannerlist{iElement,1},scannerlist{iElement,2});
-            FinalDest = fullfile(ScannerDest, 'sourcedata',scannerlist{iElement,1},scannerlist{iElement,2});
+            FinalDest = fullfile(ScannerDest, 'sourcedata',scannerlist{iElement,1});
             xASL_adm_CreateDir(FinalDest);
             xASL_Move(source,FinalDest,true); % Use the recursive option
             
