d9e5a8228a9cdb852643231840913b84d99d3bbf
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-05-14 17:34:58 +0200

    OSIPI: Update TestEffectPreSmoothing

--- OSIPI/TestEffectPreSmoothing.m
+++ OSIPI/TestEffectPreSmoothing.m
@@ -1,22 +1,29 @@
 % 1. Get ASL and M0 NIfTIs
 ExploreASL;
-pathData = '/scratch/hjmutsaerts/OSIPI_TF6.1/Synthetic';
-NIIlist = xASL_adm_GetFileList(pathData, '^.*(M0|asl)\.nii$', 'FPListRec');
+pathData = '/Users/henk/ExploreASL/OSIPI_TF6.1/Synthetic';
+folderList = xASL_adm_GetFileList(pathData, '^sub-DRO\d', 'FPList', [0 Inf], 1);
 
-for iNii=1:length(NIIlist)
-    [Fpath, Ffile] = xASL_fileparts(NIIlist{iNii});
-    pathT1 = fullfile(xASL_fileparts(NIIlist{iNii}), 'T1.nii');
-    pathSmooth = fullfile(Fpath, [Ffile '_smooth.nii']);
-    
-    % 1) reslice them back to T1w
-    xASL_spm_reslice(pathT1, NIIlist{iNii}, [], [], 1, pathSmooth, 4);
-    
-    % 2) smooth them
-    xASL_spm_smooth(pathSmooth, [5 5 5], pathSmooth);
-    
-    % 3) 
-    xASL_spm_reslice(NIIlist{iNii}, pathSmooth, [], [], 1, pathSmooth, 4);
-end    
+for iFolder=1:numel(folderList)
 
-% Re-zip all files
-xASL_adm_GzipAllFiles(pathData);
+    NIIlist = xASL_adm_GetFileList(folderList{iFolder}, '^.*(m0scan|asl)\.nii$', 'FPListRec');
+
+    for iNii=2:length(NIIlist)
+        [Fpath, Ffile] = xASL_fileparts(NIIlist{iNii});
+        pathT1 = xASL_adm_GetFileList(fullfile(fileparts(Fpath), 'anat'), '^.*T1w\.nii');
+        pathTemp = fullfile(Fpath, [Ffile '_temp.nii']);
+
+        % 1) reslice them back to T1w
+        xASL_spm_reslice(pathT1, NIIlist{iNii}, [], [], 1, pathTemp, 4);
+
+        % 2) smooth them
+        xASL_spm_smooth(pathTemp, [5 5 5], pathTemp);
+
+        % 3) 
+        xASL_spm_reslice(NIIlist{iNii}, pathTemp, [], [], 1, NIIlist{iNii}, 4);
+        % 4) Delete temp
+        xASL_delete(pathTemp);
+    end
+
+    % 5) Re-zip all files
+    xASL_adm_GzipAllFiles(folderList{iFolder});
+end
