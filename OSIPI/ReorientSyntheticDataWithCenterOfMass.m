eccc487d688e4297cc3ae5983ad4c7fdeb3c211a
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2021-05-14 11:39:39 +0200

    OSIPI: ReorientSyntheticDataWithCenterOfMass new BIDS structure

--- OSIPI/ReorientSyntheticDataWithCenterOfMass.m
+++ OSIPI/ReorientSyntheticDataWithCenterOfMass.m
@@ -1,10 +1,22 @@
-
+%% Original legacy DRO format
 Root = '/Users/henk/surfdrive/OSIPI_TF6.1/Synthetic';
 DirList = xASL_adm_GetFileList(Root, '^Dataset.*$', 'FPList', [0 Inf], 1);
 
-for iList=2:length(DirList)
+for iList=1:length(DirList)
     OtherList = xASL_adm_GetFileList(DirList{iList}, '^(M0|asl).*\.nii$', 'FPList');
     T1list = xASL_adm_GetFileList(DirList{iList}, '^T1\.nii$', 'FPList');
+
+    xASL_im_CenterOfMass(T1list{1}, OtherList);
+    xASL_adm_GzipAllFiles(DirList{iList});
+end
+
+%% New BIDS /rawdata format
+Root = '/Users/henk/ExploreASL/OSIPI_TF6.1/Synthetic';
+DirList = xASL_adm_GetFileList(Root, '^sub-DRO\d$', 'FPList', [0 Inf], 1);
+
+for iList=1:length(DirList)
+    OtherList = xASL_adm_GetFileList(fullfile(DirList{iList}, 'perf'), '^.*(m0scan|asl)\.nii$', 'FPListRec');
+    T1list = xASL_adm_GetFileList(fullfile(DirList{iList}, 'anat'), '^.*T1w\.nii$', 'FPList');
 
     xASL_im_CenterOfMass(T1list{1}, OtherList);
     xASL_adm_GzipAllFiles(DirList{iList});
