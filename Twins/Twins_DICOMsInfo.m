cb719704e8ab1a96c066ef4b5ba27524d57b5a9b
Michael Stritt <stritt.michael@gmail.com>
2021-07-23 20:18:51 +0200

    Update scripts

--- Twins/Twins_DICOMsInfo.m
+++ Twins/Twins_DICOMsInfo.m
@@ -42,10 +42,11 @@ switch username
         fprintf('Unknown user...\n');
 end
 
-
-
 % Get all sub directories
 baseDirs = xASL_adm_GetFsList(rootDir,'^.+$',true);
+
+% Get patient directory list
+patientList = getPatientDirListXML(rootDir);
 
 %% Set-up patients structure
 fprintf('Determine patients...\n');
---
+++ Twins/getPatientDirListXML.m
@@ -0,0 +1,39 @@
+function patientList = getPatientDirListXML(rootDir)
+%getPatientDirListXML Get patient dir list
+%
+% INPUT:        n/a
+%
+% OUTPUT:       n/a
+%
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% DESCRIPTION:  Get patient dir list.
+%
+%               Written by M. Stritt, 2021.
+%
+% EXAMPLE:      n/a
+% -----------------------------------------------------------------------------------------------------------------------------------------------------
+% Copyright 2015-2021 ExploreASL
+
+
+% XML
+import matlab.io.xml.dom.*
+
+% Individual directories
+subjectDirs = xASL_adm_GetFsList(rootDir,'^.+$',true);
+
+for iSubject = 1:numel(subjectDirs)
+
+	% Read XML file
+	xmlFile = fullfile(rootDir,subjectDirs{iSubject},'SECTRA','CONTENT.XML');
+	xDoc = parseFile(Parser,xmlFile);
+	allListItems = getElementsByTagName(xDoc,'listitem');
+
+
+end
+
+
+
+
+end
+
+
