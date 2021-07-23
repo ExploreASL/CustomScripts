79333fa86ec7018be729f2301b72aba70176191a
Michael Stritt <stritt.michael@gmail.com>
2021-07-23 20:34:27 +0200

    Update scripts

--- Twins/getPatientDirListXML.m
+++ Twins/getPatientDirListXML.m
@@ -24,6 +24,19 @@ for iSubject = 1:numel(subjectDirs)
 	xmlFile = fullfile(rootDir,subjectDirs{iSubject},'SECTRA','CONTENT.XML');
 	xmlStruct = xml2struct(xmlFile);
 
+	if isfield(xmlStruct,'content') && ...
+		isfield(xmlStruct.content,'patient') && ...
+		isfield(xmlStruct.content.patient,'patient_data') && ...
+		isfield(xmlStruct.content.patient.patient_data,'personal_id')
+		thisPatientID = xmlStruct.content.patient.patient_data.personal_id;
+		fprintf('%s ...\n',thisPatientID);
+	else
+		warning('It was not possible to extract the patient ID...');
+		thisPatientID = 'unknown';
+	end
+
+	patientList{iSubject,1} = thisPatientID;
+	patientList{iSubject,2} = subjectDirs{iSubject};
 
 end
 
