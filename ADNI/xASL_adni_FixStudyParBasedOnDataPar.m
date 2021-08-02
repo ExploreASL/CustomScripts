91282b1232d58f9c4a90f0d81fe180a3be715c30
MichaelStritt <stritt.michael@gmail.com>
2021-08-02 10:16:23 +0200

    Update ADNI

--- ADNI/xASL_adni_FixStudyParBasedOnDataPar.m
+++ ADNI/xASL_adni_FixStudyParBasedOnDataPar.m
@@ -25,7 +25,6 @@ function studyPar = xASL_adni_FixStudyParBasedOnDataPar(json, studyPar)
     
     if strcmpi(json.x.Vendor,'Philips')
         studyPar.ASLContext = 'deltam';
-        studyPar.SliceTiming = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
     end
     
     if strcmpi(json.x.Vendor,'Siemens')
