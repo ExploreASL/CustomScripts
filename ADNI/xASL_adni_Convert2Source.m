00d006534dd591460e40330c8f8fea2862462867
MichaelStritt <stritt.michael@gmail.com>
2021-09-16 17:39:58 +0200

    #7 Improved conversion feedback

--- ADNI/xASL_adni_Convert2Source.m
+++ ADNI/xASL_adni_Convert2Source.m
@@ -63,15 +63,23 @@ for iCase = 1:size(adniCases,1)
 end
 
 fprintf('\n====================================================================================================\n');
+% Check if there were errors
 if isempty(listFailed)
     fprintf('No errors during conversion...\n');
 else
     fprintf('Some errors during conversion...\n');
     fprintf('Please check the listFailed cell array...\n');
 end
-outputCases = xASL_adm_GetFsList(adniDirectoryResults,'^\d{3}_.+$',true);
+% Check missing cases (cases without asl or t1 are excluded, so you wont get the exact same number of input folders)
+outputCases = xASL_adm_GetFsList(adniDirectoryResults,'^\d{3}_.+$',true)';
 if size(adniCases,1)~=size(outputCases,1)
     fprintf('Number of input cases is not equal to number of output cases...\n');
+    % Print missing cases
+    for iCase=1:size(adniCases,1)
+        if ~ismember(adniCases{iCase,1},outputCases(:,1))
+            fprintf('Missing %s...\n',adniCases{iCase,1});
+        end
+    end
 end
 
 
