befb2e38a2dface4a7e4ecb2ac76484b230534c9
jan-petr <j.petr@hzdr.de>
2022-03-02 14:44:45 +0100

    PAM fix CL order for certain subject

--- PAM/importPAM.m
+++ PAM/importPAM.m
@@ -23,7 +23,32 @@ for iType = 1:length(importTypes)
 	xASL_Move(fullfile(pathRoot,['studyPar' importTypes{iType} '.json']), fullfile(pathRoot,'studyPar.json'));
 	xASL_Move(fullfile(pathRoot,['sourcedata' importTypes{iType}]), fullfile(pathRoot,'sourcedata'));
 	system(['rm -r ' fullfile(pathRoot,'derivatives','ExploreASL','lock','xASL_module_Import')]);
-	ExploreASL(pathRoot,[1 1 0 0],0);
+	
+	% For sourcedata13 and sourcedata23, we need to reorder the NII from CCCLLL to CLCLCL
+	if strcmp(importTypes{iType},'13') || strcmp(importTypes{iType},'23')
+		ExploreASL(pathRoot,[1 0 0 0],0);
+		% Go to derivatives/ExploreASL/temp a reorder everything there
+		
+		% Get all the subject names
+		subjectList = xASL_adm_GetFileList(fullfile(pathRoot,'derivatives','ExploreASL','temp'),[],false,[],true);
+		
+		% Go through all subjects
+		for iSubject = 1:length(subjectList)
+			if xASL_exist(fullfile(pathRoot,'derivatives','ExploreASL','temp',subjectList{iSubject},'ASL_1'),'dir') &&...
+				xASL_exist(fullfile(pathRoot,'derivatives','ExploreASL','temp',subjectList{iSubject},'ASL_1','ASL4D.nii'),'file')
+				% Load and reorder
+				imOld = xASL_io_Nifti2Im(fullfile(pathRoot,'derivatives','ExploreASL','temp',subjectList{iSubject},'ASL_1','ASL4D.nii'));
+				imReorder = zeros(size(imOld));
+				imReorder(:,:,:,1:2:end) = imOld(:,:,:,1:(size(imOld,4)/2));
+				imReorder(:,:,:,2:2:end) = imOld(:,:,:,(size(imOld,4)/2 + 1):end);
+				xASL_io_SaveNifti(fullfile(pathRoot,'derivatives','ExploreASL','temp',subjectList{iSubject},'ASL_1','ASL4D.nii'),...
+					              fullfile(pathRoot,'derivatives','ExploreASL','temp',subjectList{iSubject},'ASL_1','ASL4D.nii'),imReorder);
+			end
+		end
+		ExploreASL(pathRoot,[0 1 0 0],0);
+	else
+		ExploreASL(pathRoot,[1 1 0 0],0);
+	end
 	
 	% Return back
 	xASL_Move(fullfile(pathRoot,'studyPar.json'), fullfile(pathRoot,['studyPar' importTypes{iType} '.json']));
