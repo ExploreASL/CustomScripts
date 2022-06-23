f5dda7b7c6684cfb52f22c7cf1d1716a25ea775d
jan-petr <j.petr@hzdr.de>
2022-06-23 15:25:31 +0200

    PAM processing without M0

---
+++ PAM/reprocessPAMWithoutM0.m
@@ -0,0 +1,40 @@
+% Specify the data folder
+pathRoot = '/pet/projekte/asl/data/PAM/lastVersionTest';
+
+% Go through all subjects
+
+subjectList = xASL_adm_GetFileList(fullfile(pathRoot,'derivatives','ExploreASL'),'^sub-.+$',false,[],true);
+		
+% Go through all subjects
+for iSubject = 1:length(subjectList)
+	nameSubject = subjectList{iSubject};
+	
+	% Delete the M0 files
+	xASL_delete(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','M0.json'));
+	xASL_delete(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','M0.nii'));
+	xASL_delete(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','rM0.nii'));
+	
+	% Modify the ASL.json
+	% Correct M0 type
+	if xASL_exist(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','ASL4D.json'))
+		jsonASL = spm_jsonread(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','ASL4D.json'));
+		if isfield(jsonASL,'M0Type')
+			jsonASL = rmfield(jsonASL,'M0Type');
+		end
+		if isfield(jsonASL,'M0Estimate')
+			jsonASL = rmfield(jsonASL,'M0Estimate');
+		end
+		jsonASL.M0 = 'UseControlAsM0';
+		
+		% Round the BSup Time
+		jsonASL.BackgroundSuppressionPulseTime = round(jsonASL.BackgroundSuppressionPulseTime*1000)/1000;
+		spm_jsonwrite(fullfile(pathRoot,'derivatives','ExploreASL',nameSubject,'ASL_1','ASL4D.json'),jsonASL);
+	end
+	
+	% Delete the locks for the ASL module
+	xASL_delete(fullfile(pathRoot,'derivatives','ExploreASL','lock','xASL_module_ASL',nameSubject),true);
+end
+
+%%
+% Rerun the ASL module
+ExploreASL(pathRoot,0,[0 1 0]);
