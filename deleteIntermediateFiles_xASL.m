ae5e64f023d3bef6a9014437a2f10cd5cdefde52
Henk Mutsaerts <henkjanmutsaerts@gmail.com>
2023-12-18 19:42:04 +0100

    #deleteIntermediateFiles_xASL: cleanup

--- deleteIntermediateFiles_xASL.m
+++ deleteIntermediateFiles_xASL.m
@@ -14,13 +14,13 @@ popDir = fullfile(rootDir, 'Population');
 preFixList = {'rT1_ORI_' 'rT1_' 'rFLAIR_' '(m|)rc\dT1_' 'noSmooth_M0_' 'mean_control_' 'PWI_' 'SliceGradient_' 'SNR'}
 nPrefixes = length(preFixList);
 
-
+popDir = '/data/radv/radG/RAD/share/Twins/Twins_bids/Twins_ExploreASL/Population';
 
     if isunix()
-        PathToSearch = xASL_adm_UnixPath(popDir);
         for iPreFix=1:nPrefixes
-		    oldPath = pwd;
-		    [exit_code, system_result] = system(['cd ' PathToSearch '; find . -d 1 -name ' preFixList{iPreFix} '\*.nii\* -exec rm {} \;']);
+		    PathToSearch = popDir;
+            iPreFix
+		    [exit_code, system_result] = system(['cd ' PathToSearch '; find . -maxdepth 1 -name ' preFixList{iPreFix} '\*.nii\* -exec rm -f {} \;'])
         end
     end
 
